5619a8aed7c9919bf67107fbdd4262f6f5509678
Jeffreydl <mister-das-loupah@hotmail.com>
2017-04-24 21:36:42 +0200

    login pagina css enzo

--- view/login.php
+++ view/login.php
@@ -6,42 +6,42 @@
 
 <body>
 	<div class="container">
-	<div id="column-left">
+		<div class="container2">
+			<div id="column-left">
+
+				<div id="qrcode">
+					<?php echo "<img src='$QRCodeLink'>";?>
+				</div>
+
+			</div>
 		
-		<div id="qrcode">
-			<?php echo "<img src='$QRCodeLink'>";?>
+			<div id="column-right">
+				<div id="container-right">
+					<div id="container-right2">
+						<div id="logo"> <img src="view/images/logo.png" alt="Logo"></div> <br><br>
+
+						<form method="POST">
+
+							<input class="input email" type="text" name="email" required autocorrect="off" autocapitalize="off">
+							<img class="emailLogo" src="view/images/login.png">
+							
+							<input class="input password" type="password" name="password" required autocorrect="off" autocapitalize="off">
+							<img class="passwordLogo" src="view/images/slotje.png">
+							<br>
+
+							<button class="button" type="submit" name="login">Sign In</button>	
+							<span class="forgot_password"><a href="../controller/forgot_password.php">Forgot Password?</a></span>
+						</form>	
+					</div>
+				</div>
+	
+			</div>
+				<div id="helper">
+					<img class="lijn" src="view/images/lijn.png"></img>
+				</div>
+				<div id="container2"><div id="wit"></div></div>
 		</div>
+			<span id="footer">Copyright 2017 © Windesheim Flevoland</span>
 		
-	</div>
-	
-	<div id="column-right">
-		
-		<div id="logo"> <img src="view/images/logo.png" alt="Logo"></div> <br><br>
-
-		<form method="POST">
-
-			<input class="input email" type="text" name="email" required>
-			<img class="emailLogo" src="view/images/login.png">
-			<br>
-			<input class="input password" type="password" name="password" required>
-			<img class="passwordLogo" src="view/images/slotje.png">
-			<br>
-			
-			<button class="button" type="submit" name="login">Sign In</button> <br>
-			<span class="forgot_password"><a href="../controller/forgot_password.php">Forgot Password?</a></span>
-		</form>	
-
-<!-- DOUBLE BORDERS TEST
-		<div class="borders">
-		<input class="borders" type="text">
-
-		</div>
--->
-
-	</div>
-	
-		<div id="container2"><div id="wit"></div></div>
-	
-	<span id="footer">Copyright 2017 © Windesheim Flevoland</span>
 	</div>
 </body>
