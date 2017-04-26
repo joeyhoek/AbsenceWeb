b7514a7ce4847f1e58f526df945c47695bc1365c
Jeffreydl <mister-das-loupah@hotmail.com>
2017-04-26 21:05:16 +0200

    ganoumee

---
+++ view/js/border-left.js
@@ -0,0 +1,6 @@
+window.onload = function () {
+ setTimeout(function(){
+  document.getElementsByClassName('input')[0].classList.add("loaded");
+  document.getElementsByClassName('input')[1].classList.add("loaded");
+ }, 1500);
+};
--- view/login.php
+++ view/login.php
@@ -1,47 +1,48 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="view/css/login.css">
+	<link rel="stylesheet" href="view/css/login.css" media="screen and (min-width:10px)">
 </head>
 
 
 <body>
 	<div class="container">
-	<div id="column-left">
+		<div class="container2">
+			<div id="column-left">
+
+				<div id="qrcode">
+					<?php echo "<img src='$QRCodeLink'>";?>
+				</div>
+
+			</div>
 		
-		<div id="qrcode">
-			<?php echo "<img src='$QRCodeLink'>";?>
+			<div id="column-right">
+				<div id="container-right">
+					<div id="container-right2">
+						<div id="logo"> <img class="logo" src="view/images/logo.png" alt="Logo"></div> <br><br>
+
+						<form method="POST">
+
+							<input class="input email" type="text" name="email" required autocorrect="off" autocapitalize="off" spellcheck="false" autocomplete="new-password">
+							<img class="emailLogo" src="view/images/login.png">
+							
+							<input class="input password" type="password" name="password" required autocorrect="off" autocapitalize="off" spellcheck="false" autocomplete="new-password">
+							<img class="passwordLogo" src="view/images/slotje.png">
+							<br>
+
+							<button class="button" type="submit" name="login">Sign In</button>	
+							<span class="forgot_password"><a href="../controller/forgot_password.php">Forgot Password?</a></span>
+						</form>	
+					</div>
+				</div>
+	
+			</div>
+				<div id="helper">
+					<img class="lijn" src="view/images/lijn.png"></img>
+				</div>
+				<div id="container2"><div id="wit"></div></div>
 		</div>
+			<span id="footer">Copyright 2017 © Windesheim Flevoland</span>
 		
 	</div>
-	
-	<div id="column-right">
-		
-		<div id="logo"> <img src="view/images/logo.png" alt="Logo"></div> <br><br>
-
-		<form method="POST">
-
-			<input class="input email" type="text" name="email" required>
-			<img class="emailLogo" src="view/images/login.png">
-			<br>
-			<input class="input password" type="password" name="password" required>
-			<img class="passwordLogo" src="view/images/slotje.png">
-			<br>
-			
-			<button class="button" type="submit" name="login">Sign In</button> <br>
-			<span class="forgot_password"><a href="../controller/forgot_password.php">Forgot Password?</a></span>
-		</form>	
-
-<!-- DOUBLE BORDERS TEST
-		<div class="borders">
-		<input class="borders" type="text">
-
-		</div>
--->
-
-	</div>
-	
-		<div id="container2"><div id="wit"></div></div>
-	
-	<span id="footer">Copyright 2017 © Windesheim Flevoland</span>
-	</div>
+	<script src="view/js/border-left.js"></script>
 </body>
