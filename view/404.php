44bbceaa08b146fc3b4b1f6c3f34ea27463e74d5
Jeffreydl <mister-das-loupah@hotmail.com>
2017-05-04 23:52:37 +0200

    404 view

---
+++ view/404.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> </title>
+</head>
+
+<body>
+<h1>Error 404 Page not found</h1>
+
+<img src="http://i.imgur.com/9hWOGuG.png" width="300" height="auto"><br><br>
+<a href="http://absence.innovatewebdesign.nl">Go to Homepage</a>
+
+</body>
+</html>
