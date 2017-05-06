26bdc2caa562581b3cf14976f095490aab4ea1b5
joeyhoek <joeyhoek07@hotmail.com>
2017-05-06 23:45:17 +0200

    helemaal niet klaar testdingetje

--- view/js/upload.js
+++ view/js/upload.js
@@ -1,1 +1,20 @@
 // JavaScript Document
+
+var $input    = $form.find('input[type="file"]'),
+    $label    = $form.find('label'),
+    showFiles = function(files) {
+      $label.text(files.length > 1 ? ($input.attr('data-multiple-caption') || '').replace( '{count}', files.length ) : files[ 0 ].name);
+    };
+
+// ...
+
+.on('drop', function(e) {
+  droppedFiles = e.originalEvent.dataTransfer.files; // the files that were dropped
+  showFiles( droppedFiles );
+});
+
+//...
+
+$input.on('change', function(e) {
+  showFiles(e.target.files);
+});
