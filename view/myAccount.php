82fc526b518626bc14159de76b9badc4c6e99fde
Jeffreydl <mister-das-loupah@hotmail.com>
2017-05-07 01:39:56 +0200

    menu stuff

--- view/myAccount.php
+++ view/myAccount.php
@@ -2,15 +2,17 @@
 	
 <header class="cd-header">
 	<a class="cd-primary-nav-trigger" href="#0">
-		<span class="cd-menu-text">Menu</span><span class="cd-menu-icon"></span>
+		<span class="cd-menu-icon"></span>
 	</a> <!-- cd-primary-nav-trigger -->
  </header>
 
 <nav>
 	<ul class="cd-primary-nav">
-		<li class="cd-label"><img src="view/images/dashboard.png"></li>
-		<li class="cd-label"><img src="view/images/overview.png"></li>
-		<li class="cd-label"><img src="view/images/manage.png"></li>
-		<li class="cd-label"><img src="view/images/profile.png"></li>
+		<li class="cd-label"><a href="#"><img src="view/images/dashboard.png"><figcaption>Dashboard</figcaption></a></li>
+		<li class="cd-label"><a href="#"><img src="view/images/overview.png"><figcaption>Overview</figcaption></a></li>
+		<li class="cd-label"><a href="#"><img src="view/images/manage.png"><figcaption>Manage</figcaption></a></li>
+		<li class="cd-label"><a href="#"><img src="view/images/profilePicture.png"><figcaption>Hello Colin!</figcaption></a></li>
+		<li class="cd-label"><a href="#"><img src="view/images/logout.png"></a></li>
 	</ul>
+	<a href="#"><img class="questionMark" src="view/images/questionMark.png"></a>
 </nav>
