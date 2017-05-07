1394cd2763018e1083ecea87dc79560fd37d2541
Jeffreydl <mister-das-loupah@hotmail.com>
2017-05-07 20:24:38 +0200

    menu chabnges

--- view/myAccount.php
+++ view/myAccount.php
@@ -1,5 +1,3 @@
-	<h1>INGELOGD ENZO <a href="/?action=logout">Logout</a></h1>
-	
 <header class="cd-header">
 	<a class="cd-primary-nav-trigger" href="#0">
 		<span class="cd-menu-icon"></span>
@@ -8,11 +6,15 @@
 
 <nav>
 	<ul class="cd-primary-nav">
-		<li class="cd-label"><a href="#"><img src="view/images/dashboard.png"><figcaption>Dashboard</figcaption></a></li>
-		<li class="cd-label"><a href="#"><img src="view/images/overview.png"><figcaption>Overview</figcaption></a></li>
-		<li class="cd-label"><a href="#"><img src="view/images/manage.png"><figcaption>Manage</figcaption></a></li>
-		<li class="cd-label"><a href="#"><img src="view/images/profilePicture.png"><figcaption>Hello Colin!</figcaption></a></li>
-		<li class="cd-label"><a href="#"><img src="view/images/logout.png"></a></li>
+		<li class="cd-label"><a href="#"><img class="imgs" src="view/images/dashboard.png"><figcaption>Dashboard</figcaption></a></li>
+		<li class="cd-label"><a href="#"><img class="imgs" src="view/images/overview.png"><figcaption>Overview</figcaption></a></li>
+		<li class="cd-label"><a href="#"><img class="imgs" src="view/images/manage.png"><figcaption>Manage</figcaption></a></li>
+		
+		<div class="profile">
+			<li class="cd-label"><a href="#"><img class="imgs" src="view/images/profilePicture.png"><figcaption>Hello Colin!</figcaption></a></li>
+			<a href="/?action=logout"><img class="imgs logout" src="view/images/logout.png"></a>
+		</div>
+		<a href="#"><img class="questionMark" src="view/images/questionMark.png"></a>
 	</ul>
-	<a href="#"><img class="questionMark" src="view/images/questionMark.png"></a>
+
 </nav>
