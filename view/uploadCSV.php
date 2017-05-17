2f34ee2999519b31510445c9918ca003dd32b237
joeyhoek <joeyhoek07@hotmail.com>
2017-05-17 23:55:52 +0200

    upload csv

--- view/uploadCSV.php
+++ view/uploadCSV.php
@@ -1,32 +1,90 @@
 <?php
 
-  $upload_success = null;
-  $upload_error = '';
+	use Team10\Absence\Model\Encryption as Encryption;
+	use Team10\Absence\Model\Connection as Connection;
 
-  if (!empty($_FILES['files'])) {
-    /*
-      the code for file upload;
-      $upload_success – becomes "true" or "false" if upload was unsuccessful;
-      $upload_error – an error message of if upload was unsuccessful;
-    */
-  }
+	function in_array_r($needle, $haystack, $strict = false) {
+    foreach ($haystack as $item) {
+        if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+	function readCSV($filename) {
+		if (($handle = fopen($filename, "r")) !== false) {
+			$filesize = filesize($filename);
+			$aData = array();
+			$count = 0;
+			while (($data = fgetcsv($handle, $filesize, ";")) !== false) {
+				if ($count == 0) {
+					for($i = 0; $i < count($data); $i++) {
+						$columns[$i] = $data[$i];
+					}
+				} else {
+					if (in_array_r(preg_split('/\s+/', $data[1])[0], $aData)) {
+						continue;
+					}
+					for($i = 0; $i < count($data); $i++) {
+						for ($j = 0; $j < count($columns); $j++) {
+							 $aData[$count - 1][$columns[$j]] = $data[$j];
+						}
+					}
+				}
+				$count++;
+			}
+			fclose($handle);
+			for ($i = 0; $i < count($aData); $i++) {
+				$aData[$i]["Studentnummer"] = "s" . $aData[$i]["Stud.nr."];
+				if ($aData[$i]["Tussenvoegsel"] !== "-") {
+					$aData[$i]["Achternaam"] = $aData[$i]["Tussenvoegsel"] . " " . $aData[$i]["Achternaam"];
+				}
+				unset($aData[$i]["Tussenvoegsel"]);
+			}
+			return $aData;
+		}
+	}
+
+	function uploadStudents($students) {
+		foreach ($students as $student) {
+			$connection = new Connection(DBHOST, DBUSER, DBPASS, DBNAME);
+			$encryption = new Encryption;
+			$username = htmlspecialchars($student["Studentnummer"]);
+			$firstname = $encryption->encrypt($student["Roepnaam"]);
+			$lastname = $encryption->encrypt($student["Achternaam"]);
+			$email = $encryption->encrypt($student["E-mailadres"]);
+			
+			if (!$connection->query("SELECT firstname FROM users WHERE id = '" . $username . "'")) {
+				$connection->query("INSERT INTO users VALUES ('" . $username . "', '0', '" . $firstname . "', '" . $lastname . "', '" . $email . "', '1', '1', '1', '1', '0', '0')");
+			} else {
+				//$connection->query("INSERT INTO users FROM users WHERE id = '" . $username . "'");
+			}
+		}
+		
+		return(true);
+	}
+
+	if (isset($_FILES['csv'])) {
+		$filename = $_FILES['csv']['name'];
+		$ext = pathinfo($filename, PATHINFO_EXTENSION);
+		if ($ext == "csv") {
+			$students = readCSV($_FILES['csv']['tmp_name']);
+			if (!uploadStudents($students)) {
+				echo "failure";
+			} else {
+				echo "gelukt";
+			}
+		} else {
+			echo "Wrong file type. File must be a .csv file";
+		}
+	}
 
 ?>
   
 
-  <form class="box" method="post" action="" enctype="multipart/form-data">
-
-  <?php if ($upload_success === null): ?>
-
-  <div class="box__input">
-    <!-- ... -->
-  </div>
-
-  <?php endif; ?>
-
-  <!-- ... -->
-
-  <div class="box__success"<?php if( $upload_success === true ): ?> style="display: block;"<?php endif; ?>>Done!</div>
-  <div class="box__error"<?php if( $upload_success === false ): ?> style="display: block;"<?php endif; ?>>Error! <span><?=$upload_error?></span>.</div>
-
+<form  method="post" enctype="multipart/form-data">
+ 	Select image to upload:
+    <input type="file" name="csv" accept=".csv" required>
+    <input type="submit" value="Upload">
 </form>
