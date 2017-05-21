5b635a75a312c105710fd79d35d338a90d27fc21
Jeffreydl <mister-das-loupah@hotmail.com>
2017-05-21 20:15:36 +0200

    qq

--- view/newCourse.php
+++ view/newCourse.php
@@ -1,24 +1,28 @@
 <div class="container">
 
 
-	<div class="container2">
-		<div id="column-left">
-			<p class="importCourse">Course imported from schedule:</p>
-			<p class="course">Nederlands</p>
-			
+	<div class="container2 container3">
+		<div id="column-left2">
+			<div class="importCourse">Course imported from schedule:</div><br /><br />
+			<div class="course">Nederlands</div>
+			<img class="start" src="view/images/start.png">
+			<div class="startCourse">Start Course</div>
 		
 		</div>
 
 		<div id="column-right">
 			<div id="container-right">
 				<div id="container-right2">
+					<input class="hoi" type="text" name="searchCourse">
+					
+					
 					
 						
 				</div>
 			</div>
 
-		</div>
-			<div id="helper">
+		</div>	
+			<div id="helper2">
 				<img class="lijn" src="view/images/lijn.png"></img>
 			</div>
 			
