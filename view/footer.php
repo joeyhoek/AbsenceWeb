d177beb73e7a56636ee57ea90085903a041a1c4b
joeyhoek <joeyhoek07@hotmail.com>
2017-05-26 01:10:33 +0200

    heel veel dinggen

--- view/footer.php
+++ view/footer.php
@@ -1,24 +1,7 @@
 	<span id="footer">Copyright 2017 © Windesheim Flevoland</span>
-<<<<<<< HEAD
-<<<<<<< HEAD
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/border-left.js?v=56"></script>
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/jquery-2.1.1.js?v=455434"></script>
+	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/border-left.js?v=131531"></script>
 	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/main.js?v=455434"></script>
 	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/modernizr.js?v=455434"></script>
 	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/buttons.js?v=45"></script>
-=======
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/border-left.js?v=45556434"></script>
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/jquery-2.1.1.js?v=455434"></script>
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/main.js?v=455434"></script>
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/modernizr.js?v=455434"></script>
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/buttons.js?v=35"></script>
->>>>>>> origin/master
-=======
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/border-left.js?v=56"></script>
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/jquery-2.1.1.js?v=455434"></script>
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/main.js?v=455434"></script>
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/modernizr.js?v=455434"></script>
-	<script src="<?php echo PROTOCOL . DOMAIN . ROOT; ?>view/js/buttons.js?v=45"></script>
->>>>>>> origin/master
 </body>
 </html>
